7dd05014ca4348dfd111562ab38abdd02ba722b8
Максим Долженко <maxim_2003@mail.ru>
2023-05-02 15:18:06 +0300

    Add dto's

--- HolidayCalculator/src/main/java/ru/neoflex/holidaycalculator/dto/HolidayRequest.java
+++ HolidayCalculator/src/main/java/ru/neoflex/holidaycalculator/dto/HolidayRequest.java
@@ -1,10 +1,12 @@
 package ru.neoflex.holidaycalculator.dto;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@RequiredArgsConstructor
 public class HolidayRequest {
     private final BigDecimal salary;
     private final int daysCount;
--- HolidayCalculator/src/main/java/ru/neoflex/holidaycalculator/dto/HolidayResponse.java
+++ HolidayCalculator/src/main/java/ru/neoflex/holidaycalculator/dto/HolidayResponse.java
@@ -1,10 +1,13 @@
 package ru.neoflex.holidaycalculator.dto;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@RequiredArgsConstructor
 public class HolidayResponse {
     private final BigDecimal money;
 }
